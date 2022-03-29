a8d93ad762a0103093da1b85f826b6bca750e130
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2022-03-29 18:40:13 +0400

    Revert "initial support for jupyter notebook"

--- src/ssm/shell.py
+++ src/ssm/shell.py
@@ -1,19 +1,19 @@
 import time
 import instance
-from typing import Tuple, Union, Any
+from typing import Tuple
 
 
-def shell(instance_name: str, *commands: str, **kwargs: str) -> Union[Tuple[str, str], Tuple[Any, str, str]]:
+def shell(instance_name: str, *commands: str, **kwargs: str) -> Tuple[str, str]:
     """
     Execure shell script over ssm channel
 
     Args:
         instance_name (str): Virtual Machine instance name
         *commands (str): (optional) list of additional arguments to be passed to ssm
-        **kwargs (str): (optional) classifiers, at the moment, profile name, delay (dafult 5), number of attempts (20), whether to wait for completion
+        **kwargs (str): (optional) classifiers, at the moment, profile name, delay (dafult 5), number of attempts (20)
 
     Returns:
-        standard output, standard err if wait=True (default), otherwise ssm_client, command_id, instance_id
+        standard output, standard err
 
     """
     session, instance_id = instance.start(instance_name, **kwargs)
@@ -29,27 +29,24 @@ def shell(instance_name: str, *commands: str, **kwargs: str) -> Union[Tuple[str,
         }
     )
     command_id = result.Command.CommandId
+    waiter = ssm_client.get_waiter('command_executed')
     # see https://stackoverflow.com/questions/50067035/retrieving-command-invocation-in-aws-ssm
     time.sleep(2)
-    if kwargs.get('wait', True):
-        waiter = ssm_client.get_waiter('command_executed')
-        try:
-            waiter.wait(
-                CommandId=command_id,
-                InstanceId=instance_id,
-                WaiterConfig={
-                    'Delay': kwargs.get('delay', 5),
-                    'MaxAttempts': kwargs.get('attempts', 20)
-                }
-            )
-        finally:
-            result = ssm_client.get_command_invocation(
-                CommandId=command_id,
-                InstanceId=instance_id,
-                PluginName='aws:RunShellScript'
-            )
-            print(result.StandardOutputContent)
-            print(result.StandardErrorContent)
-        return result.StandardOutputContent, result.StandardErrorContent
-    else:
-        return ssm_client, command_id, instance_id
+    try:
+        waiter.wait(
+            CommandId=command_id,
+            InstanceId=instance_id,
+            WaiterConfig={
+                'Delay': kwargs.get('delay', 5),
+                'MaxAttempts': kwargs.get('attempts', 20)
+            }
+        )
+    finally:
+        result = ssm_client.get_command_invocation(
+            CommandId=command_id,
+            InstanceId=instance_id,
+            PluginName='aws:RunShellScript'
+        )
+        print(result.StandardOutputContent)
+        print(result.StandardErrorContent)
+    return result.StandardOutputContent, result.StandardErrorContent
