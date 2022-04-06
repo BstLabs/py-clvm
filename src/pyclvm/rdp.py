58fa2673712e9e2bf076aa3d9fec2c981ee89b9d
Dmitry Slobodchikov <zoosman@mail.ru>
2022-04-06 16:33:13 +0300

    Do facilities for MacOS (darwin)

--- src/pyclvm/rdp.py
+++ src/pyclvm/rdp.py
@@ -1,4 +1,6 @@
 import subprocess
+import time
+from distutils.log import warn
 from sys import platform
 from typing import Dict, Final, Tuple
 
@@ -12,7 +14,12 @@ _SYSTEM_RDP_CLIENT: Final[Dict[str, Tuple[str, ...]]] = {
         "+toggle-fullscreen",
         "/v:localhost:{local_port}",
     ),
-    "darwin": (),
+    "darwin": (
+        "/Applications/Microsoft Remote Desktop.app/Contents/MacOS/Microsoft Remote Desktop",
+        ">",
+        "/dev/null",
+        "2>&1",
+    ),
 }
 _RDP_PORT: Final[str] = "3389"
 
@@ -38,7 +45,14 @@ def rdp(instance_name: str, **kwargs: str) -> None:
     local_port = redirect.start(instance_name, **kwargs)
 
     # launch rdp
+    if platform == "darwin":
+        warn(
+            "\nCreate profile connection manually, server: localhost, port: 3389, or copy link rdp://localhost:3389\n"
+        )
+        time.sleep(10)
+
     subprocess.run([s.format(local_port=local_port) for s in cmd])
 
     # stop port redirection
     redirect.stop(instance_name, **kwargs)
+
--- src/pyclvm/vscode/install.py
+++ src/pyclvm/vscode/install.py
@@ -1,6 +1,8 @@
+import os
 import platform
 import subprocess
 import sys
+from pathlib import Path
 from typing import List
 
 # TODO: form it at install time- no reason to detect platform dynamically
@@ -30,36 +32,83 @@ def install() -> None:
 
     """
     env = platform.uname()
-    assert "Linux" == env.system
-    assert "Ubuntu" in env.version
-    assert "x86_64" == env.machine
-    _run_script(
-        ["sudo", "apt", "update"],
-        [
-            "sudo",
-            "apt",
-            "install",
-            "-y",
-            "software-properties-common",
-            "apt-transport-https",
-            "wget",
-        ],
-        [
-            "wget",
-            "-q",
-            "https://packages.microsoft.com/keys/microsoft.asc",
-            "-O",
-            "/tmp/microsoft.asc",
-        ],
-        ["sudo", "apt-key", "add", "/tmp/microsoft.asc"],
-        [
-            "sudo",
-            "add-apt-repository",
-            "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main",
-        ],
-        ["sudo", "apt", "install", "-y", "code"],
-        ["code", "--install-extension", "ms-vscode.makefile-tools"],
-        ["code", "--install-extension", "ms-python.python"],
-        ["code", "--install-extension", "ms-vscode-remote.remote-ssh"],
-        ["code", "--install-extension", "valentjn.vscode-ltex"],
-    )
+    if env.system == "Linux":
+        # assert "Linux" == env.system
+        assert "Ubuntu" in env.version
+        assert "x86_64" == env.machine
+        _run_script(
+            ["sudo", "apt", "update"],
+            [
+                "sudo",
+                "apt",
+                "install",
+                "-y",
+                "software-properties-common",
+                "apt-transport-https",
+                "wget",
+            ],
+            [
+                "wget",
+                "-q",
+                "https://packages.microsoft.com/keys/microsoft.asc",
+                "-O",
+                "/tmp/microsoft.asc",
+            ],
+            ["sudo", "apt-key", "add", "/tmp/microsoft.asc"],
+            [
+                "sudo",
+                "add-apt-repository",
+                "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main",
+            ],
+            ["sudo", "apt", "install", "-y", "code"],
+            ["code", "--install-extension", "ms-vscode.makefile-tools"],
+            ["code", "--install-extension", "ms-python.python"],
+            ["code", "--install-extension", "ms-vscode-remote.remote-ssh"],
+            ["code", "--install-extension", "valentjn.vscode-ltex"],
+        )
+    # --- MacOS install script wia brew ---
+    elif env.system == "Darwin":
+
+        home = os.path.expanduser("~")
+        bash_profile = f"{home}/.bash_profile"
+        brew_path = "/opt/homebrew/bin/brew"
+        vscode_path = "/opt/homebrew/bin/code"
+        pyqt_path = "/opt/homebrew/opt/pyqt5/lib/python3.9/site-packages"
+
+        if not Path(brew_path).is_file():
+            _run_script(
+                [
+                    "/bin/bash",
+                    "-c",
+                    '"$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"',
+                ]
+            )
+
+        if Path(vscode_path).is_file():
+            _run_script(
+                ["brew", "install", "visual-studio-code"],
+                ["brew", "install", "qt5", "pyqt5"],
+            )
+
+            export_line = f"export PYTHONPATH=$PYTHONPATH:{pyqt_path}\n"
+            flag = False
+
+            with open(bash_profile, "r+", encoding="utf8") as file:
+                for line in file.readlines():
+                    if line == export_line:
+                        flag = True
+                        break
+
+            if not flag:
+                with open(bash_profile, "a", encoding="utf8") as file:
+                    file.write(export_line)
+
+            sys.path.append(pyqt_path)
+
+            _run_script(
+                ["code", "--install-extension", "ms-vscode.makefile-tools"],
+                ["code", "--install-extension", "ms-python.python"],
+                ["code", "--install-extension", "ms-vscode-remote.remote-ssh"],
+                ["code", "--install-extension", "valentjn.vscode-ltex"],
+            )
+
