dfd59a1f536bf17affbd4d18426327db91165b8d
Lior Kashti <48313697+lkashti@users.noreply.github.com>
2022-04-20 14:41:28 +0300

    you can now use instance id to close all its session (#59)

--- src/pyclvm/ssm/session/stop.py
+++ src/pyclvm/ssm/session/stop.py
@@ -1,7 +1,7 @@
 from pyclvm._common.session import get_session
 
 
-def stop(session_id: str, **kwargs: str) -> None:
+def stop(identifier: str, **kwargs: str) -> None:
     """
     Terminate ssm session
 
@@ -15,4 +15,11 @@ def stop(session_id: str, **kwargs: str) -> None:
     """
     session = get_session(kwargs)
     client = session.client("ssm")
-    client.terminate_session(SessionId=session_id)
+    if identifier[0:3] == "i-0":  # close all sessions with a certain vm
+        active_sessions = client.describe_sessions(
+            State="Active", Filters=[{"key": "Target", "value": identifier}]
+        )
+        for session_id in (s["SessionId"] for s in active_sessions["Sessions"]):
+            client.terminate_session(SessionId=session_id)
+    else:  # close a single session
+        client.terminate_session(SessionId=identifier)
