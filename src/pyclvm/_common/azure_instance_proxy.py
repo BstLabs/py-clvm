8858c0b16a8d32c8d8a8baecc4fceaec577ff3d5
Dmitry Slobodchikov <zoosman@mail.ru>
2022-07-05 13:41:50 +0300

    Add Azure instance connect socket

--- src/pyclvm/_common/azure_instance_proxy.py
+++ src/pyclvm/_common/azure_instance_proxy.py
@@ -1,10 +1,16 @@
 # -*- coding: utf-8 -*- #
+
 import os
-from time import sleep
-import subprocess
-from multiprocessing import Process, ProcessError
-from typing import Any, Iterable, Union
 import signal
+import socket
+import subprocess
+import sys
+
+# from multiprocessing import Process, ProcessError
+from select import select
+from threading import Thread, ThreadError
+from time import sleep
+from typing import Any, Iterable, Union
 
 from .session_azure import AzureSession
 
@@ -74,10 +80,14 @@ class AzureRemoteShellProxy(AzureInstanceProxy):
     # ---
     def execute(self, *commands: Union[str, Iterable], **kwargs) -> Any:
         _connector = AzureRemoteConnector(self)
-        _executor = AzureRemoteExecutor(self, _connector, *commands, **kwargs) if len(*commands) > 0 else AzureRemoteSocket(self, _connector, **kwargs)
+        _executor = (
+            AzureRemoteExecutor(self, _connector, *commands, **kwargs)
+            if len(*commands) > 0
+            else AzureRemoteSocket(self, _connector, **kwargs)
+        )
         _connector.start()
         _executor.start()
-        print("Socket started")
+        # print("Starting tunnel")
         _connector.join()
         _executor.join()
 
@@ -88,7 +98,7 @@ class AzureRemoteShellProxy(AzureInstanceProxy):
 
 
 # ---
-class AzureRemoteConnector(Process):
+class AzureRemoteConnector(Thread):
     """
     Azure remote connector class
     """
@@ -123,17 +133,15 @@ class AzureRemoteConnector(Process):
                 resource_group,
                 "--target-resource-id",
                 f"/subscriptions/{subscription}/resourceGroups/{resource_group}/providers/Microsoft.Compute/virtualMachines/{instance_name}",
+                "--only-show-errors",
             ]
-            self._proc = subprocess.call(cmd)
-        except (ProcessError, RuntimeError):
+            self._proc = subprocess.call(cmd, stdout=subprocess.DEVNULL)
+        except (ThreadError, RuntimeError):
             raise
-
-    # def __del__(self):
-    #     self.kill()
 
 
 # ---
-class AzureRemoteExecutor(Process):
+class AzureRemoteExecutor(Thread):
     """
     Azure remote socket class
     """
@@ -152,10 +160,8 @@ class AzureRemoteExecutor(Process):
 
     # ---
     def run(self):
-        sleep(5)
-        command = (
-            " ".join(self._commands[0]) if len(self._commands) > 0 else ""
-        )  # TODO check "commands" tuple
+        sleep(3)
+        command = " ".join(*self._commands) if len(self._commands) > 0 else ""
         try:
             cmd = [
                 "ssh",
@@ -168,17 +174,18 @@ class AzureRemoteExecutor(Process):
                 "UserKnownHostsFile=/dev/null",
                 "-o",
                 "StrictHostKeyChecking=no",
-                command,
             ]
+            if command:
+                cmd.append(command)
             subprocess.run(cmd)
             os.killpg(os.getpgid(self._connector.pid), signal.SIGTERM)
             self.close()
-        except (ProcessError, RuntimeError):
+        except (ThreadError, RuntimeError):
             raise
 
 
 # ---
-class AzureRemoteSocket(Process):
+class AzureRemoteSocket(Thread):
     """
     Azure remote socket class
     """
@@ -195,13 +202,74 @@ class AzureRemoteSocket(Process):
 
     # ---
     def run(self):
-        cmd = [
-            "nc",
-            "localhost",
-            "22026",
-        ]
         sleep(5)
-        subprocess.run(cmd)
-        os.killpg(os.getpgid(self._connector.pid), signal.SIGTERM)
+        # cmd = [
+        #     "nc",
+        #     "localhost",
+        #     "22026",
+        # ]
+        # subprocess.run(cmd)
+        TcpProxy("127.0.0.1", 22026)()
+        # os.killpg(os.getpgid(self._connector.pid), signal.SIGTERM)
+        self._connector.pid.join()
         self.close()
 
+
+# ---
+class TcpProxy:
+    BUFFER_SIZE = 4096
+
+    def __init__(self, dst_host, dst_port):
+        self._dst = (dst_host, dst_port)
+        self._input_list = []
+
+        self._proxy_in, self._proxy_out = self._setup_proxy()
+        self._target = self._setup_target()
+
+    def _setup_target(self):
+        try:
+            target = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            target.connect(self._dst)
+            self._input_list.append(target)
+            return target
+        except ConnectionRefusedError as e:
+            print(e)
+            sys.exit(1)
+
+    def _setup_proxy(self):
+        proxy_in = sys.stdin
+        proxy_out = sys.stdout
+        self._input_list.append(proxy_in)
+        return proxy_in, proxy_out
+
+    def __call__(self):
+        while True:
+            channel_rlist, _, _ = select(self._input_list, [], [])
+
+            for channel in channel_rlist:
+                if channel == self._proxy_in:
+                    data = channel.buffer.read1()
+                    if len(data):
+                        self._send(data)
+                elif channel == self._target:
+                    _data = channel.recv(self.BUFFER_SIZE)
+                    if len(_data) == 0:
+                        self._close(channel)
+                        break
+                    else:
+                        self._receive(_data)
+
+    def _close(self, _channel):
+        self._input_list.remove(_channel)
+        self._target.close()
+
+    def _send(self, data):
+        try:
+            self._target.send(data)
+        except OSError as e:
+            print(e)
+            sys.exit(1)
+
+    def _receive(self, data):
+        self._proxy_out.buffer.write(data)
+        self._proxy_out.buffer.flush()
