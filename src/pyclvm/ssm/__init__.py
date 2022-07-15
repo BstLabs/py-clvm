3b8100bd228034ff3678f21d2ad7d3a47a07c1da
Shahriyar Rzayev <rzayev.sehriyar@gmail.com>
2022-07-15 13:07:57 +0400

    Provide retry mechanism for failed MFA code; added extra command for removing credentials files (#117)
    * Changed the credentials path; added new command ssm clear to wipe out credentials file; skipping if FileNotFoundError raised
    
    * Provided backoff mechanics for retrying in case of MFA fails

--- src/pyclvm/ssm/__init__.py
+++ src/pyclvm/ssm/__init__.py
@@ -1,6 +1,7 @@
 """session manager utilities"""
 
 from . import session
+from .clear import clear
 from .shell import shell
 
-__all__ = ["shell", "session"]
+__all__ = ["shell", "session", "clear"]
